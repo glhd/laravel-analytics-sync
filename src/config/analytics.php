69bef39d1e3d7bc6d71a181570b40b8bba82e6e6
Junior Grossi <juniorgro@gmail.com>
2016-08-26 12:39:56 -0300

    Add the configuration file

---
+++ src/config/analytics.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Laravel Analytics Sync config file
+ *
+ * @package Galahad\LaravelAnalyticsSync
+ */
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | User Model
+    |--------------------------------------------------------------------------
+    |
+    | This is the model that should be used as User, where the column to store
+    | the Google Analytics Client ID should be stored.
+    |
+    */
+    'model' => 'App\User',
+    /*
+    |--------------------------------------------------------------------------
+    | Google Analytics Client ID column name
+    |--------------------------------------------------------------------------
+    |
+    | This is the column name on the User model, where the Google Analytics
+    | Client ID is stored.
+    |
+    */
+    'column' => 'analytics_client_id',
+];
